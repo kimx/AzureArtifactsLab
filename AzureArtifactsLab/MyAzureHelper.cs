abf4dbee7b9bac2c9ca4adc5186191c135fcc476
kim <kim@bud4.net>
2020-03-12 11:21:04 +0800

    init

---
+++ AzureArtifactsLab/MyAzureHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureArtifactsLab
+{
+    public class MyAzureHelper
+    {
+        public static string HelloWorld()
+        {
+            return "HelloWorld";
+        }
+    }
+}
